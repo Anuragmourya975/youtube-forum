9267773de03de01fffd857792431a869e0ac3765
Aarti Jethaniya <jethaniya20aarti@gmail.com>
2022-02-09 15:11:54 +0530

    project structure

---
+++ src/Components/Component.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Card from '../Components/Card/Card';
+import Login from '../Components/Login/Login';
+import Navbar from '../Components/Navbar/Navbar';
+
+const Component = () => {
+  return (
+    <div>
+        <Login/>
+        <Navbar/>
+        <Card/>
+    </div>
+  )
+}
+export default Component;
