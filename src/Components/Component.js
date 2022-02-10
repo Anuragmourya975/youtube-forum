8354ea4214f7a6ac739b86f7ff2422c1672949fe
Anurag mourya <anuragmourya975@gmail.com>
2022-02-10 11:17:29 +0530

    card added

--- src/Components/Component.js
+++ src/Components/Component.js
@@ -1,18 +1,18 @@
-import React from 'react'
-import Card from '../Components/Card/Card';
-import Login from '../Components/Login/Login';
-import Header from '../Components/Header/Header';
-import Sidebar from '../Components/Sidebar/Sidebar'
-import './Component.css'
+import React from "react";
+import Login from "../Components/Login/Login";
+import Header from "../Components/Header/Header";
+import Sidebar from "../Components/Sidebar/Sidebar";
+import "./Component.css";
+import Card101 from "./Card/Card101";
 const Component = () => {
   return (
-    <div className='component'>
-        <Header/>
-        <div className='component-page'>
-            <Sidebar />
-            <Card/>
-        </div>
+    <div className="component">
+      <Header />
+      <div className="component-page">
+        <Sidebar />
+        <Card101 />
+      </div>
     </div>
-  )
-}
+  );
+};
 export default Component;
